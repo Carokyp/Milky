fe36640983400f6b67a73ee5f05ce45739cd5ea5
Carokyp <97530091+Carokyp@users.noreply.github.com>
2026-05-05 15:24:00 +0100

    Customize admin

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'flavor',
+        'sku',
+        'price',
+        'stock',
+        'product_image')
+    ordering = ('sku',)
+    list_filter = ('is_available',)
+
+
+admin.site.register(Product, ProductAdmin)
