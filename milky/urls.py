ac6bcd97c846311f0852f4e728195e6fcf348d2b
Carokyp <97530091+Carokyp@users.noreply.github.com>
2026-05-26 10:50:07 +0100

    Add accounts app: create model for countries, update settings and urls

--- milky/urls.py
+++ milky/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
