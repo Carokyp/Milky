948582c4e51f5c820d8b80e3125f2d695636f44b
Carokyp <97530091+Carokyp@users.noreply.github.com>
2026-05-26 13:00:16 +0100

    Add models for Country, Customer, User_customer, and Contact; register models in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Country, Customer, User_customer, Contact
 
 # Register your models here.
+
+admin.site.register(Country)
+admin.site.register(Customer)
+admin.site.register(User_customer)
+admin.site.register(Contact)
--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,46 @@
 from django.db import models
 
 
-class Countries(models.Model):
+class Country(models.Model):
     name = models.CharField(max_length=255)
+    iso_code = models.CharField(max_length=2)
+
+    class Meta:
+        verbose_name_plural = 'Countries'
 
     def __str__(self):
         return self.name
+
+
+class Customer(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
+
+class User_customer(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    enabled = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.customer}"
+
+
+class Contact(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    ip_address = models.GenericIPAddressField()
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
