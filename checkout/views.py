58a049e725644fd360b973cf83a52b107f0e8bbf
Carokyp <97530091+Carokyp@users.noreply.github.com>
2026-05-30 20:55:20 +0100

    Refactor cart functionality: remove old cart template, implement new cart template, and enhance checkout view with order form initialization and empty cart handling.

--- checkout/views.py
+++ checkout/views.py
@@ -1,14 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from accounts.forms import CustomerForm
+from django.contrib import messages
+from django.urls import reverse
+from .forms import OrderForm
 from accounts.models import UserCustomer
 
 
 @login_required
 def checkout(request):
 
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, 'Your cart is empty.')
+        return redirect(reverse('products'))
+
     user_customers = UserCustomer.objects.filter(user=request.user).first()
     customer = user_customers.customer if user_customers else None
-    form = CustomerForm(instance=customer)
 
-    return render(request, 'checkout/checkout.html', {'form': form})
+    order_form = OrderForm(initial={
+        'delivery_name': customer.name if customer else '',
+        'delivery_surname': customer.surname if customer else '',
+        'delivery_phone': customer.phone_number if customer else '',
+        'delivery_address': customer.address if customer else '',
+        'delivery_city': customer.city if customer else '',
+        'delivery_county': customer.county if customer else '',
+        'delivery_postcode': customer.postal_code if customer else '',
+        'delivery_country': customer.country if customer else '',
+    })
+
+    return render(request, 'checkout/checkout.html', {'form': order_form})
