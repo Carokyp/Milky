99027fd1a022a6e0c48063869a17fd134b98c039
Carokyp <97530091+Carokyp@users.noreply.github.com>
2026-05-31 10:27:45 +0100

    Add fallback image for products without an image and include media context processor

--- milky/settings.py
+++ milky/settings.py
@@ -85,6 +85,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # Required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'cart.context_processors.cart_contents',
             ],
             'builtins': [
