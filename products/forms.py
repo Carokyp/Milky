dac9fbbcd1d8925364c9293210511e42e33e293b
Carokyp <97530091+Carokyp@users.noreply.github.com>
2026-06-13 08:19:53 +0100

    feat: integrate custom image widget in product forms and templates

--- products/forms.py
+++ products/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from .models import Product
+from .widgets import CustomClearableFileInput
 
 
 class ProductForm(forms.ModelForm):
+    product_image = forms.ImageField(
+        label='Image',
+        required=False,
+        widget=CustomClearableFileInput()
+    )
 
     class Meta:
         model = Product
@@ -14,15 +20,18 @@ class ProductForm(forms.ModelForm):
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
 
-        # SKU en lecture seule
-        self.fields['sku'].widget.attrs['readonly'] = True
-        self.fields['sku'].widget.attrs['class'] = 'form-control text-muted'
+        # SKU field should be read-only in the form
+        if 'sku' in self.fields:
+            self.fields['sku'].widget.attrs['readonly'] = True
+            self.fields['sku'].widget.attrs['class'] = 'form-control text-muted'
 
         # other customisations
-        self.fields['is_available'].label = 'Available for sale'
-        self.fields['is_available'].widget.attrs['class'] = 'form-check-input'
-        self.fields['description'].widget.attrs.update({
-            'rows': 4,
-            'placeholder': 'Enter product description here...',
-            'class': 'form-control',
-        })
+        if 'is_available' in self.fields:
+            self.fields['is_available'].label = 'Available for sale'
+            self.fields['is_available'].widget.attrs['class'] = 'form-check-input'
+        if 'description' in self.fields:
+            self.fields['description'].widget.attrs.update({
+                'rows': 4,
+                'placeholder': 'Enter product description here...',
+                'class': 'form-control',
+            })
