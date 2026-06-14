88862d972dbea5eb5b5eea4080f6502b70ced98a
Carokyp <97530091+Carokyp@users.noreply.github.com>
2026-06-14 18:18:00 +0100

    feat: add display_name field to Customer model

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,7 @@ class Customer(models.Model):
     # Personal info
     name = models.CharField(max_length=255, blank=True)
     surname = models.CharField(max_length=255, blank=True)
+    display_name = models.CharField(max_length=50, blank=True, null=True)
     phone_number = models.CharField(max_length=20, blank=True)
 
     # Address
