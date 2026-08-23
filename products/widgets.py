b044051da111488a039507641e96e84394f0c33b
Carokyp <97530091+Carokyp@users.noreply.github.com>
2026-08-23 17:34:56 +0100

    Add docstrings, wrap long line, drop unused gettext_lazy wrapper

--- products/widgets.py
+++ products/widgets.py
@@ -1,16 +1,20 @@
 import os
 
 from django.forms.widgets import ClearableFileInput
-from django.utils.translation import gettext_lazy as _
 
 
 class CustomClearableFileInput(ClearableFileInput):
-    clear_checkbox_label = _('Remove Image')
-    initial_text = _('Current Image')
-    input_text = _('')
-    template_name = 'products/custom_widget_templates/custom_clearable_file_input.html'
+    """File input widget showing the current file's name, clearable."""
+
+    clear_checkbox_label = 'Remove Image'
+    initial_text = 'Current Image'
+    input_text = ''
+    template_name = (
+        'products/custom_widget_templates/custom_clearable_file_input.html'
+    )
 
     def get_context(self, name, value, attrs):
+        """Add the current file's basename to the widget context."""
         context = super().get_context(name, value, attrs)
         if value:
             context['widget']['filename'] = os.path.basename(value.name)
