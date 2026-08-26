b5a39f1a461b9644debb549b3af34c50ab76675a
Carokyp <97530091+Carokyp@users.noreply.github.com>
2026-08-26 14:13:07 +0100

    Add AWS S3 storage configuration and update requirements for django-storages

--- milky/settings.py
+++ milky/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     "django_countries",
     "crispy_forms",
     "crispy_bootstrap5",
+    "storages",
 
     # Local apps
     "home",
@@ -190,6 +191,25 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+if 'USE_AWS' in os.environ:
+    # AWS S3 settings
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = "milky-static"
+    AWS_REGION_NAME = "us-east-1"
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+    # Static and media files storage settings
+    STATICFILES_STORAGE = "custom_storages.StaticStorage"
+    STATICFILES_LOCATION = "static"
+    DEFAULT_FILE_STORAGE = "custom_storages.MediaStorage"
+    MEDIAFILES_LOCATION = "media"
+
+    # override static and media URLs in production
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
+
 
 # Free delivery threshold
 FREE_DELIVERY_THRESHOLD = 25.00
