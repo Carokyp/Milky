cef065a7090ee5e3c53cd42fbfd849a36dcff712
Carokyp <97530091+Carokyp@users.noreply.github.com>
2026-08-26 09:21:57 +0100

    Prepare project for Heroku deployment
    Add Procfile and .python-version, support DATABASE_URL env var in
    settings.py alongside local sqlite3 fallback, and add Heroku app URL
    to ALLOWED_HOSTS.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn milky.wsgi:application
--- milky/settings.py
+++ milky/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists("env.py"):
     # Load environment variables such as SECRET_KEY from env.py.
@@ -33,6 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
+    "milky-app-839f694f035d.herokuapp.com",  # Heroku app URL
 ]
 
 
@@ -133,12 +135,17 @@ WSGI_APPLICATION = "milky.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
