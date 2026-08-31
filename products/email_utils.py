14baacab1d185f62f89e92627860870eeaa4aa78
Carokyp <97530091+Carokyp@users.noreply.github.com>
2026-08-31 13:07:01 +0100

    Use single_image_url in the toast mini-cart, gift picker and gift email

--- products/email_utils.py
+++ products/email_utils.py
@@ -13,9 +13,7 @@ def build_gift_email_context(
         "contact": contact,
         "personal_message": personal_message,
         "product_image_url": request.build_absolute_uri(
-            product.product_image.url
-            if product.product_image
-            else static("images/no-image.png")
+            product.single_image_url or static("images/no-image.png")
         ),
         **build_email_font_urls(request),
     }
